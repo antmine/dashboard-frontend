046a458231a67ac0cd714e6c116e05ffc241f857
Francois <frdebellescize@hotmail.fr>
2017-06-09 13:06:05 +0200

    clean

--- src/app/models/client.ts
+++ src/app/models/client.ts
@@ -1,13 +1,13 @@
 export class Client {
-  constructor(
-    public name: string,
-    public lastname: string,
-    public email: string,
-    public birthday: string,
-    public password: string,
-    public street: string,
-    public city: string,
-    public zip_code: string,
-    public country: string
-  ) {  }
+  public name: string;
+  public lastname: string;
+  public email: string;
+  public birthday: string;
+  public password: string;
+  public street: string;
+  public city: string;
+  public zip_code: string;
+  public country: string;
+
+  constructor() {  }
 }
