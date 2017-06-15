bbe07a4d0cb60a6fa7250d25be430a8e292dfc8e
bertho-h <hugoberthon@hotmail.fr>
2017-06-15 11:21:50 +0200

    deleted the *mes sites* placeholder part of the side nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,28 +18,21 @@ export class AppComponent {
     router.events.subscribe((event: RouterEvent) => {
       this.navigationInterceptor(event);
     });
+  }
 
-    this.http.get("https://api.github.com/users/karma151/repos").map(response => response.json()).subscribe((res) =>{
-      this.data = res;
-    },
-    (err) => console.log(err),
-    () => {}
-  );
-}
-
-navigationInterceptor(event: RouterEvent): void {
-  if (event instanceof NavigationStart) {
-    this.loading = true;
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.loading = false;
+    }
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.loading = false;
+    }
   }
-  if (event instanceof NavigationEnd) {
-    this.loading = false;
-  }
-  // Set loading state to false in both of the below events to hide the spinner in case a request fails
-  if (event instanceof NavigationCancel) {
-    this.loading = false;
-  }
-  if (event instanceof NavigationError) {
-    this.loading = false;
-  }
-}
 }
