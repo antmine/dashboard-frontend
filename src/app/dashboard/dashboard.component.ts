299bf316c49731bc4058e1a0061a419bead06d9f
bertho-h <hugoberthon@hotmail.fr>
2017-06-22 13:41:35 +0200

    init push

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit, Injectable } from '@angular/core';
+import {CookieService} from "angular2-cookie/core";
+import {RequestOptions,Headers, Http, Response} from '@angular/http';
+import {Router}                                       from "@angular/router";
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [CookieService]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() {}
+  constructor(
+      private _cookieService: CookieService,
+      private router: Router,
+      private http: Http,
+  ) {
+    if (!this._cookieService.get("token"))
+      this.router.navigate(["/login"]);
+  }
 
   ngOnInit() {
   }
