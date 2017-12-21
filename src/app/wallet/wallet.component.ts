a2346b230ee1a9ec223f7232c8016c26a7621412
bertho-h <hugoberthon@hotmail.fr>
2017-12-21 17:43:48 +0100

    get bitcoin amount of client

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { LoginRedirectionService } from "app/service/login-redirection/login-redirection.service";
+
 
 import {
 	RequestOptions,
@@ -20,15 +22,19 @@ import { LoaderService } from "../service/loader.service";
 	selector: "app-wallet",
 	templateUrl: "./wallet.component.html",
 	styleUrls: ["./wallet.component.css"],
+	providers: [LoginRedirectionService]
 })
 export class WalletComponent implements OnInit {
 	client = new Client();
+	public data;
+	public bitcoinAmount = 0;
 
 	constructor(
 		private http: Http,
 		private router: Router,
 		public snackBar: MatSnackBar,
-		private loaderService: LoaderService
+		private loaderService: LoaderService,
+		private loginRedirectionService: LoginRedirectionService,
 	) {}
 
 	ngOnInit() {}
@@ -38,26 +44,55 @@ export class WalletComponent implements OnInit {
 	}
 
 	private displayBitcoin(){
-		//this.loaderService.displayLoader(true);
-		/*let url = "http://back.dashboard.antmine.io/client";
+		this.loaderService.displayLoader(true);
+		let url = "http://back.dashboard.antmine.io/website";
 		let headers = new Headers({ "Content-Type": "application/json" });
 		let options = new RequestOptions({
 			headers: headers,
 			withCredentials: true
 		});
 		this.http
+		.get(url, options)
+		.map(response => response.json())
+		.subscribe(
+			res => {
+				this.data = res;
+				console.log(this.data);
+				this.getAmountBitcoin(this.data);
+			},
+			err => {
+				console.log("GET request error: " + err);
+				this.loginRedirectionService.checkStatus(err);
+			},
+			() => {}
+		);
+	}
+
+	private getAmountBitcoin(data) {
+		console.log("getting all websites");
+		for (var i = 0; i < data.length; ++i) {
+			console.log(this.data[i].ID_WEBSITE);
+			let url = "http://back.dashboard.antmine.io/website/" + this.data[i].ID_WEBSITE;
+			let headers = new Headers({ "Content-Type": "application/json" });
+			let options = new RequestOptions({
+				headers: headers,
+				withCredentials: true
+			});
+			this.http
 			.get(url, options)
 			.map(response => response.json())
 			.subscribe(
 				res => {
 					this.loaderService.displayLoader(false);
-			},
+					this.bitcoinAmount += res.BITCOIN_AMOUNT;
+					console.log(this.bitcoinAmount);
+				},
 				err => {
-				console.log("GET request error: " + err);
-				this.loginRedirectionService.checkStatus(err);
-				this.loaderService.displayLoader(false);
-			}
-			);*/
+					this.loaderService.displayLoader(false);
+					this.loginRedirectionService.checkStatus(err)
+				}
+			);
+		}
 	}
 
 	onSubmit() {
@@ -74,10 +109,11 @@ export class WalletComponent implements OnInit {
 			.subscribe(
 				data => {
 					this.loaderService.displayLoader(false);
-					this.snackBar.open("Votre transfert à été réalisé", "Ok");
+					this.snackBar.open("Le transfert a été réalisé", "Ok");
 				},
 				err => {
 					console.log("POST request error: " + err);
+					this.snackBar.open("Le transfert a échoué", "Ok");
 					this.loginRedirectionService.checkStatus(err);
 					this.loaderService.displayLoader(false);
 				}
